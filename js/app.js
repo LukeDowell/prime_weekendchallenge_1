f97203658aae023ff54c57eafdac41b30ba43a15
Luke Dowell <lukedowell@gmail.com>
2015-07-18 18:37:13 -0500

    Added alphabetic sorting, colored review indicators and cleaned up unneccessary code in app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     //.submit was acting strangely so I switched to a standard event handler
     $("#submitButton").on('click', function(event) {
         event.preventDefault(); //prevent a page refresh
-        var $form = $("#entryForm");
 
         //Check to see if we already have an employee with the provided ID
         var empId = $("[name='empNum']").val();
@@ -25,6 +24,7 @@ $(document).ready(function() {
             employees.push(emp);
             var employeeCard = new EmployeeCard(emp);
             employeeCards.push(employeeCard);
+            insertCard(employeeCard);
         } else {
 
             //We already have an employee with that ID
@@ -49,6 +49,16 @@ $(document).ready(function() {
         employees.push(emp);
         var employeeCard = new EmployeeCard(emp);
         employeeCards.push(employeeCard);
+        insertCard(employeeCard);
+    });
+
+    $("#closeButton").on('click', function(event) {
+        event.preventDefault();
+        $("#entryOverlay").hide();
+    });
+
+    $("header").on('click', function() {
+        $("#entryOverlay").show();
     });
 });
 
@@ -56,9 +66,9 @@ var employees = []; //Where we store our employees
 var employeeCards = []; //Where we store our employee cards
 
 //Our arrays containing 'random' information
-var FIRST_NAMES = ["Susan", "Sandy", "Salmon", "Sorbet", "Shakira", "Rafi", "Rufus", "Rondo", "Mazer", "Mack", "Mistriss"];
-var LAST_NAMES = ["O'Mally", "O'Donnel", "O'SayCanYouSee", "Smith" , "Rackham", "Dowell", "Alan", "Ender", "Valentine"];
-var OCCUPATION = ["Painter", "Printer", "Police-r", "Programmer", "Podcaster", "Mailman", "Pool Boy", "Ninja", "Spy"];
+var FIRST_NAMES = ["Abraham", "Bob", "Carl", "Dan", "Ebert", "Frank", "George", "Heather", "Io", "Susan", "Sandy", "Salmon", "Sorbet", "Shakira", "Rafi", "Rufus", "Rondo", "Mazer", "Mack", "Mistriss"];
+var LAST_NAMES = ["Abbot", "Barf", "Constantine", "Dillenger", "Eckhart", "O'Mally", "O'Donnel", "O'SayCanYouSee", "Smith" , "Rackham", "Dowell", "Alan", "Ender", "Valentine"];
+var OCCUPATION = ["Painter", "Printer", "Police-r", "Programmer", "Podcaster", "Mailman", "Pool Boy", "Ninja", "Spy", "Pidgeon", "Consumer", "Buyer", "Seller"];
 
 /**
  * An employee object
@@ -92,48 +102,39 @@ function Employee(first, last, id, title, review, salary) {
  */
 function EmployeeCard(employee) {
     //Pull out all their information into paragraph elements
-    var $pName = $("<p/>", {
+    this.$pName = $("<p/>", {
         class: "employeeName",
         text: employee.firstName + " " + employee.lastName
     });
 
-    var $pId = $("<p/>", {
+    this.$pId = $("<p/>", {
         class: "employeeId",
         text: employee.id
     });
 
-    var $pTitle = $('<p/>', {
+    this.$pTitle = $('<p/>', {
         class: "employeeTitle",
         text: employee.title
     });
 
-    var $pSalary = $('<p/>', {
+    this.$pSalary = $('<p/>', {
         class: "employeeSalary",
         text: employee.salary
     });
 
-    var $pReview = $('<p/>', {
+    this.$pReview = $('<p/>', {
         class: "employeeReview",
+        style: "color: " + getReviewColor(employee.review),
         text: "Review: " + employee.review
     });
 
-    var $card = $("<div/>", {
-        class: "employeeCard"
-    });
-
-    var $cardHeader = $("<div/>", {
+    this.$cardHeader = $("<div/>", {
         class: "employeeCardHeader"
     });
 
-    $cardHeader.append($pName);
-    $cardHeader.append($pId);
-
-    $card.append($cardHeader);
-    $card.append($pTitle);
-    $card.append($pSalary);
-    $card.append($pReview);
-
-    $(".employeeCardContainer").append($card);
+    this.$card = $("<div/>", {
+        class: "employeeCard"
+    });
 }
 
 /**
@@ -152,9 +153,41 @@ function generateEmployee() {
         LAST_NAMES[getRandomInt(0, LAST_NAMES.length)],
         randomId,
         OCCUPATION[getRandomInt(0, OCCUPATION.length)],
-        getRandomInt(0, 6),
+        getRandomInt(1, 6),
         getRandomInt(0, 100000)
     )
+}
+
+/**
+ * Inserts an employee card into our container alphabetically
+ * @param employeeCard
+ */
+function insertCard(employeeCard) {
+
+    var added = false;
+
+    employeeCard.$cardHeader.append(employeeCard.$pName);
+    employeeCard.$cardHeader.append(employeeCard.$pId);
+
+    employeeCard.$card.append(employeeCard.$cardHeader);
+    employeeCard.$card.append(employeeCard.$pTitle);
+    employeeCard.$card.append(employeeCard.$pSalary);
+    employeeCard.$card.append(employeeCard.$pReview);
+
+    $(".employeeCardContainer > .employeeCard > .employeeCardHeader > .employeeName").each(function() {
+        var currentCardName = $(this).text().split(" ")[0]; //Get the first name
+        var newCardName = employeeCard.$pName.text().split(" ")[0]; //Get the first name
+        console.log(currentCardName + " " + newCardName);
+        if((newCardName < currentCardName || newCardName === currentCardName) && !added) {
+            console.log("1");
+            $(this).parent().parent().before(employeeCard.$card);
+            added = true;
+        }
+    });
+
+    if(!added) {
+        $(".employeeCardContainer").append(employeeCard.$card);
+    }
 }
 
 /**
@@ -185,4 +218,30 @@ function findEmployeeById(id) {
  */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/**
+ * Gives a color based on review score
+ * @param review
+ *      The review score
+ * @returns {string}
+ *      A string color value
+ */
+function getReviewColor(review) {
+    var color = "red";
+    switch(review) {
+        case 5:
+            color = "gold";
+            break;
+        case 4:
+            color = "green";
+            break;
+        case 3:
+            color = "brown";
+            break;
+        case 2:
+            color = "orange";
+            break;
+    }
+    return color;
 }
